3f37d12fd91643735e045e7f5a9b2dcb07748092
Aditya Shenoy <adityashenoy49@gmail.com>
2020-01-15 21:31:13 +0530

    .

--- pre_processing.py
+++ pre_processing.py
@@ -5,28 +5,62 @@ from algorithm import rotate
 # or cube rotations move from algorithm
 def clean_algo(algo):
   
-  # Replace all wide turn moves to their corresponding single slice move and whole cube rotation
-  algo = algo.replace("r'", "L' x'")
-  algo = algo.replace("r2", "L2 x2")
-  algo = algo.replace("r", "L x")
-  algo = algo.replace("l'", "R' x")
-  algo = algo.replace("l2", "R2 x2")
-  algo = algo.replace("l", "R x'")
-  algo = algo.replace("u'", "D' y'")
-  algo = algo.replace("u2", "D2 y2")
-  algo = algo.replace("u", "D y")
-  algo = algo.replace("d'", "U' y")
-  algo = algo.replace("d2", "U2 y2")
-  algo = algo.replace("d", "U y'")
-  algo = algo.replace("f'", "B' z'")
-  algo = algo.replace("f2", "B2 z2")
-  algo = algo.replace("f", "B z")
-  algo = algo.replace("b'", "F' z")
-  algo = algo.replace("b2", "F2 z2")
-  algo = algo.replace("b", "F z'")
+  # Split the algo into its moves separated by space
+  algo_split = algo.split(' ')
 
-  # Split moves on space
-  algo = algo.split(' ')
+  # This mapping is to replace wide turn moves into their corresponding 2 slice moves
+  wide_to_2_slice = {
+    "r'": "R' M",
+    "r2": "R2 M2",
+    "r" : "R M'",
+    "l'": "L' M'",
+    "l2": "L2 M2",
+    "l" : "L M",
+    "u'": "U' E",
+    "u2": "U2 E2",
+    "u" : "U E'",
+    "d'": "D' E'",
+    "d2": "D2 E2",
+    "d" : "D E",
+    "f'": "F' S'",
+    "f2": "F2 S2",
+    "f" : "F S",
+    "b'": "B' S",
+    "b2": "B2 S2",
+    "b" : "B S'",
+  }
+
+  # If the first or last move is a wide turn move, then replace it with above mapping
+  algo_split[0] = wide_to_2_slice.get(algo_split[0], algo_split[0])
+  algo_split[-1] = wide_to_2_slice.get(algo_split[-1], algo_split[-1])
+
+  # This mapping is to replace wide turn moves into their corresponding single slice move and rotation
+  wide_to_slice_rotate = {
+    "r'": "L' x'",
+    "r2": "L2 x2",
+    "r" : "L x",
+    "l'": "R' x",
+    "l2": "R2 x2",
+    "l" : "R x'",
+    "u'": "D' y'",
+    "u2": "D2 y2",
+    "u" : "D y",
+    "d'": "U' y",
+    "d2": "U2 y2",
+    "d" : "U y'",
+    "f'": "B' z'",
+    "f2": "B2 z2",
+    "f" : "B z",
+    "b'": "F' z",
+    "b2": "F2 z2",
+    "b" : "F z'",
+  }
+
+  # Replace all wide turn moves to their corresponding single slice move and whole cube rotation
+  algo_split = list(map(lambda x: wide_to_slice_rotate.get(x, x), algo_split))
+
+  # Join and split moves on space
+  algo_split = ' '.join(algo_split).split(' ')
 
   # Initialize empty list of strings for result
   res = []
@@ -35,7 +69,7 @@ def clean_algo(algo):
   op = []
 
   # Iterate through all moves in the algorithm
-  for i, e in enumerate(algo):
+  for i, e in enumerate(algo_split):
 
     # If the move is a rotation move
     if e[0] in 'xyz':
@@ -52,10 +86,10 @@ def clean_algo(algo):
         # Rotate the move
         for _ in range({2: 1}.get(len(o), 3)):
           
-          algo[i] = rotate(algo[i])[o[0]]
+          algo_split[i] = rotate(algo_split[i])[o[0]]
       
       # Append the move to the result
-      res.append(algo[i])
+      res.append(algo_split[i])
   
   # Return cleaned algo
   return ' '.join(res)
