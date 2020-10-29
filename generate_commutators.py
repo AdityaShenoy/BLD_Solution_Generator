d5fb76c7723f26844a125fc7309eb20f24fe2c72
AdityaShenoy <adityashenoy49@gmail.com>
2020-10-29 12:59:08 +0530

    .

--- generate_commutators.py
+++ generate_commutators.py
@@ -1,45 +1,63 @@
+import clipboard
+
+adjacents = {
+    'R': 'UEDFSB',
+    'M': 'UEDFSB',
+    'L': 'UEDFSB',
+    'U': 'RMLFSB',
+    'E': 'RMLFSB',
+    'D': 'RMLFSB',
+    'F': 'RMLUED',
+    'S': 'RMLUED',
+    'B': 'RMLUED'
+}
+
+all_slices = 'RMLUEDFSB'
+
+quarter_turns = ['', '\'']
+
+quarter_half_turns = ['', '2', '\'']
+
+inverse = {
+    '': '\'',
+    '2': '2',
+    '\'': ''
+}
+
+parallels = {
+    'R': 'ML',
+    'M': 'RL',
+    'L': 'RM',
+    'U': 'ED',
+    'E': 'UD',
+    'D': 'UE',
+    'F': 'SB',
+    'S': 'FB',
+    'B': 'FS'
+}
+
+edge_interchanges = {
+    'R': ['MNOP', 'BTVJ'],
+    'M': ['AIUS', 'CKWQ'],
+    'L': ['EFGH', 'DLXR'],
+    'U': ['ABCD', 'EQMI'],
+    'E': ['FJNR', 'HLPT'],
+    'D': ['UVWX', 'GKOS'],
+    'F': ['IJKL', 'CPUF'],
+    'S': ['BOXE', 'DMVG'],
+    'B': ['QRST', 'AHWN'],
+}
+
 with open('output/commutators.csv', 'w') as commutators:
-    for a1 in 'RMLUEDFSB':
-        for a1_rot in ['', '\'']:
-            for a2 in {
-                'R': 'UEDFSB',
-                'M': 'UEDFSB',
-                'L': 'UEDFSB',
-                'U': 'RMLFSB',
-                'E': 'RMLFSB',
-                'D': 'RMLFSB',
-                'F': 'RMLUED',
-                'S': 'RMLUED',
-                'B': 'RMLUED'
-            }[a1]:
-                for a2_rot in ['', '2', '\'']:
-                    a1_rot_inv = {
-                        '': '\'',
-                        '2': '2',
-                        '\'': ''
-                    }[a1_rot]
-                    a2_rot_inv = {
-                        '': '\'',
-                        '2': '2',
-                        '\'': ''
-                    }[a2_rot]
-                    for b in {
-                        'R': 'ML',
-                        'M': 'RL',
-                        'L': 'RM',
-                        'U': 'ED',
-                        'E': 'UD',
-                        'D': 'UE',
-                        'F': 'SB',
-                        'S': 'FB',
-                        'B': 'FS'
-                    }[a2]:
-                        for b_rot in ['', '2', '\'']:
-                            b_rot_inv = {
-                                '': '\'',
-                                '2': '2',
-                                '\'': ''
-                            }[b_rot]
+    for a1 in all_slices:
+        for a1_rot in quarter_turns:
+            for a2 in adjacents[a1]:
+                for a2_rot in quarter_half_turns:
+                    a1_rot_inv = inverse[a1_rot]
+                    a2_rot_inv = inverse[a2_rot]
+                    for b in parallels[a2]:
+                        for b_rot in quarter_half_turns:
+                            b_rot_inv = inverse[b_rot]
                             commutators.write(f'({a1}{a1_rot} '
                                               f'{a2}{a2_rot} '
                                               f'{a1}{a1_rot_inv}) ')
